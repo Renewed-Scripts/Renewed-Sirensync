eff92828a4fadd79e06ea7293844d2aa5d5831b8
FjamZoo <laurids112@live.dk>
2023-05-22 16:51:53 +0200

    Renewed Sirensync

---
+++ Config.lua
@@ -0,0 +1,8 @@
+return {
+
+    addonHorns = {
+        [`firetruk`] = 'VEHICLES_HORNS_FIRETRUCK_WARNING'
+    },
+
+    sirenShutOff = true -- Set to true if you want the siren to automatically shut off when the player exits the vehicle
+}
