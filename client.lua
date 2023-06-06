336361b60d84a16e1073e3c7d990d9d1240227de
FjamZoo <34508112+FjamZoo@users.noreply.github.com>
2023-06-06 21:04:35 +0200

    Fix issues with sirens and horns becomming stuck
    Test stuff

--- client.lua
+++ client.lua
@@ -21,7 +21,6 @@ local IsEntityDead = IsEntityDead
 local StopSound = StopSound
 local ReleaseSoundId = ReleaseSoundId
 local AddStateBagChangeHandler = AddStateBagChangeHandler
-local GetEntityFromStateBagName = GetEntityFromStateBagName
 local NetworkGetEntityOwner = NetworkGetEntityOwner
 local SetVehicleHasMutedSirens = SetVehicleHasMutedSirens
 local SetVehicleSiren = SetVehicleSiren
@@ -99,6 +98,7 @@ lib.onCache('vehicle', function(value)
     if cache.seat ~= -1 then return end
 
     local oldVeh = cache.vehicle
+
     if GetVehicleClass(oldVeh) ~= 18 then return end
 
     local state = Entity(oldVeh).state
@@ -119,14 +119,27 @@ end)
 -- Statebags & Keybinds --
 local function stateBagWrapper(keyFilter, cb)
     return AddStateBagChangeHandler(keyFilter, '', function(bagName, _, value, _, replicated)
-        local entity = GetEntityFromStateBagName(bagName)
+        local netId = tonumber(bagName:gsub('entity:', ''), 10)
+
+        local timeout = GetGameTimer() + 1500
+
+        while not NetworkDoesEntityExistWithNetworkId(netId) do
+            if timeout < GetGameTimer() then
+                print('Renewed-Sirensync: Timeout while waiting for entity to exist')
+                return
+            end
+
+            Wait(0)
+        end
+
+        local entity = NetToVeh(netId)
 
         local amOwner = NetworkGetEntityOwner(entity) == cache.playerId
-        if (not amOwner and replicated) or (amOwner and not replicated) then return end
-
-        if entity and entity ~= 0 then
-            cb(entity, value)
+        if (not amOwner and replicated) or (amOwner and not replicated) then
+            return
         end
+
+        cb(entity, value)
     end)
 end
 
@@ -194,27 +207,30 @@ local policeHorn = lib.addKeybind({
 
         if not state.stateEnsured then return end
 
-        didSiren = true
-        restoreSiren = state.sirenMode
-        state:set('sirenMode', 0, true)
+        if state.sirenMode == 0 then
+            restoreSiren = state.sirenMode
+            state:set('sirenMode', 0, true)
+        end
 
-        state:set('horn', true, true)
+        state:set('horn', not state.horn, true)
     end,
     onReleased = function()
         if not cache.vehicle then return end
 
+        if GetVehicleClass(cache.vehicle) ~= 18 then return end
+
         local state = Entity(cache.vehicle).state
-        state:set('horn', false, true)
 
+        SetTimeout(0, function()
+            if state.horn then
+                state:set('horn', false, true)
+            end
 
-        if not didSiren or restoreSiren == 0 then return end
-
-        if state.lightsOn then
-            state:set('sirenMode', restoreSiren, true)
-        end
-
-        restoreSiren = 0
-        didSiren = false
+            if state.lightsOn and state.sirenMode == 0 and restoreSiren > 0 then
+                state:set('sirenMode', restoreSiren, true)
+                restoreSiren = 0
+            end
+        end)
     end,
 })
 
@@ -264,7 +280,6 @@ local sirenToggle = lib.addKeybind({
 
 
 local Rpressed = false
-
 lib.addKeybind({
     name = 'sirenCycle',
     description = 'Press this button to cycle through your sirens',
@@ -278,17 +293,18 @@ lib.addKeybind({
 
         PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
 
-        if state.sirenMode == 0 then
+        if state.sirenMode == 0 and not Rpressed then
+            local sirenMode = state.sirenMode > 0 and 0 or 1
 
-            state:set('sirenMode', 1, true)
+            state:set('sirenMode', sirenMode, true)
 
             sirenToggle:disable(true)
             policeLights:disable(true)
             policeHorn:disable(true)
 
             Rpressed = true
-        else
-            local newSiren = state.sirenMode == 3 and 1 or state.sirenMode + 1
+        elseif state.sirenMode > 0 and state.lightsOn and not Rpressed then
+            local newSiren = state.sirenMode + 1 > 3 and 1 or state.sirenMode + 1
 
             state:set('sirenMode', newSiren, true)
         end
@@ -302,10 +318,16 @@ lib.addKeybind({
         policeLights:disable(false)
         policeHorn:disable(false)
 
-        if cache.vehicle then
-            Entity(cache.vehicle).state:set('sirenMode', 0, true)
-        end
+        if not cache.vehicle then return end
 
-        Rpressed = false
+        SetTimeout(0, function()
+            local state = Entity(cache.vehicle).state
+
+            if state.sirenMode > 0 then
+                state:set('sirenMode', 0, true)
+            end
+
+            Rpressed = false
+        end)
     end
 })
